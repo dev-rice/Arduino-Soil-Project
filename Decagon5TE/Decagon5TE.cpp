e43d900ec1c8b373553f0023d5ff652c58ce98bf
Chris Rice <chrisrice93@gmail.com>
2013-05-17 03:02:16 -0600

    Fixed the weird library issue

--- Decagon5TE/Decagon5TE.cpp
+++ Decagon5TE/Decagon5TE.cpp
@@ -1,8 +1,8 @@
 /*
-  Decagon5TE.cpp - Library for reading data from a Decagon 5TE.
-  Created by Chris A. Rice, May 17, 2013.
-  Released into the public domain.
-*/
+ Decagon5TE.cpp - Library for reading data from a Decagon 5TE.
+ Created by Chris A. Rice, May 17, 2013.
+ Released into the public domain.
+ */
 
 #include "Arduino.h"
 #include "Decagon5TE.h"
@@ -15,9 +15,9 @@ Decagon5TE::Decagon5TE(){
 Decagon5TE::Decagon5TE(int excitation_pin, int serial_control_pin){
     this->excitation_pin = excitation_pin;
     this->serial_control_pin = serial_control_pin;
-
+    
     ready_for_update = true;
-
+    
     pinMode(excitation_pin, OUTPUT);
     pinMode(serial_control_pin, OUTPUT);
 	
@@ -33,25 +33,24 @@ void Decagon5TE::readData(){
 	char incoming_byte = '/0';
 	
 	exciteSensor();
-
-	while (Serial.available() > 0){		
+    
+	while (Serial.available() > 0){
 		incoming_byte = Serial.read();
 		data[data_index] = incoming_byte;
 		++data_index;
 		Serial.print(incoming_byte);
 	}
-
-	//Something is wrong with these	
+    
 	dielectric_permittivity = calculateDielectricPermittivty(data);
     electrical_conductivity = calculateElectricalConductivity(data);
    	temperature = calculateTemperature(data);
 	
 	cycleSerialLine();
-
+    
 }
 
 void Decagon5TE::exciteSensor(){
-	Serial.println("Exciting Sensor");	
+	Serial.println("Exciting Sensor");
 	
 	digitalWrite(excitation_pin, LOW);
 	delay(200);
@@ -72,97 +71,91 @@ void Decagon5TE::cycleSerialLine(){
 }
 
 double Decagon5TE::calculateDielectricPermittivty(char data[]) {
-  char permittivity[4] = {'/0'};
-  int perm_index = 0;
-  int index = 0;
-  
-  int raw_dielectric = 0;
-  
-  while (data[index] != ' '){
+    String permittivity = "";
+    int index = 0;
+    
+    int raw_dielectric = 0;
+    
+    while (data[index] != ' '){
+        ++index;
+    }
     ++index;
-  }
-  ++index;
-  
-  while (data[index] != ' '){
-    permittivity[perm_index] = data[index];
-    ++perm_index;
-    ++index;
-  }
-  
-  raw_dielectric = atoi(permittivity);
-  
-  if (raw_dielectric != 4095){
-    return (double)raw_dielectric / 50.0;
-  }
-  else {
-    return 4095;
-  }
+    
+    while (data[index] != ' '){
+        permittivity += data[index];
+        ++index;
+    }
+    
+    raw_dielectric = permittivity.toInt();
+    
+    if (raw_dielectric != 4095){
+        return (double)raw_dielectric / 50.0;
+    }
+    else {
+        return 4095;
+    }
     //return 42;
 }
 double Decagon5TE::calculateElectricalConductivity(char data[]) {
-  char conductivity[4] = {'/0'};
-  int cond_index = 0;
-  int index = 0;
-  
-  int raw_conductivity = 0;
-  
-  for (int i = 0; i < 2; ++i){
-    while (data[index] != ' '){
-      ++index;
+    String conductivity = "";
+    int index = 0;
+    
+    int raw_conductivity = 0;
+    
+    for (int i = 0; i < 2; ++i){
+        while (data[index] != ' '){
+            ++index;
+        }
+        ++index;
     }
-    ++index;
-  }
-  
-  while (data[index] != ' '){
-    conductivity[cond_index] = data[index];
-    ++cond_index;
-    ++index;
-  }
-  
-  raw_conductivity = atoi(conductivity);
-  
-  if (raw_conductivity <= 700){
-    return (double)raw_conductivity / 100.0;
-  }
-  else if (raw_conductivity != 1023) {
-    return (double)(700 + 5 * (raw_conductivity - 700)) / 100;
-  }
-  else {
-    return 1023;
-  }
-    //return 42;
+    
+    while (data[index] != ' '){
+        conductivity += data[index];
+        ++index;
+    }
+    
+    raw_conductivity = conductivity.toInt();
+    
+    if (raw_conductivity <= 700){
+        return (double)raw_conductivity / 100.0;
+    }
+    else if (raw_conductivity != 1023) {
+        return (double)(700 + 5 * (raw_conductivity - 700)) / 100;
+    }
+    else {
+        return 1023;
+    }
+    return 42;
 }
 
 double Decagon5TE::calculateTemperature(char data[]) {
-  char temperature[4] = {'/0'};
-  int temp_index = 0;
-  int index = 0;
-  
-  int raw_temperature = 0;
-  
-  for (int i = 0; i < 3; ++i){
-    while (data[index] != ' '){
-      ++index;
+    String temperature = "";
+    int index = 0;
+    
+    int raw_temperature = 0;
+    
+    for (int i = 0; i < 3; ++i){
+        while (data[index] != ' '){
+            ++index;
+        }
+        ++index;
     }
-    ++index;
-  }
-  
-  while (data[index] != 'z'){
-    temperature[temp_index] = data[index];
-    ++temp_index;
-    ++index;
-  }
-  
-  raw_temperature = atoi(temperature);
-  
-  if (raw_temperature > 900 && raw_temperature != 1023){
-    raw_temperature = 900.0 + 5.0 * (raw_temperature - 900.0);
-  }
-  else if (raw_temperature == 1023) {
-    return 1023;
-  }
-  
-  double celsius = (double)(raw_temperature - 400) / 10.0;
-  return celsius;
-    //return 42;
+    
+    while (data[index] != 'z'){
+        temperature += data[index];
+        
+        ++index;
+    }
+    
+    raw_temperature = temperature.toInt();
+    
+    if (raw_temperature > 900 && raw_temperature != 1023){
+        raw_temperature = 900.0 + 5.0 * (raw_temperature - 900.0);
+    }
+    else if (raw_temperature == 1023) {
+        return 1023;
+    }
+    
+    double celsius = (double)(raw_temperature - 400) / 10.0;
+    return celsius;
 }
