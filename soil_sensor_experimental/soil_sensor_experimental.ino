cf6f48b06d2c6400b7e8bf667afb591f319c4287
Chris Rice <chrisrice93@gmail.com>
2013-05-21 18:56:30 -0600

    Current working version of the project but it runs out of memoery

--- soil_sensor_experimental/soil_sensor_experimental.ino
+++ soil_sensor_experimental/soil_sensor_experimental.ino
@@ -4,10 +4,11 @@
 #include <SPI.h>
 
 Decagon5TE sensor(2, 3, 15000);
-const int SD_CHIP_SELECT = 4;
-const int RTC_CHIP_SELECT = 8;
+const byte SD_CHIP_SELECT = 4;
+const byte RTC_CHIP_SELECT = 8;
 
-const char FILENAME[] = "5TE_Data.txt";
+char TO_UPLOAD_FILENAME[] = "test.csv";
+char ARCHIVE_FILENAME[] = "dtest.csv";
 
 char ssid[] = "Apartment";
 char pass[] = "tenretni";
@@ -19,44 +20,41 @@ WiFiClient client;
 
 void setup(){
   Serial.begin(1200); // opens serial port, sets data rate to 1200 bps as per the 5TE's spec.
-  
-  connectToNetwork(ssid, pass); 
-  
+
+  //connectToNetwork(ssid, pass);
+
   SD_init();
-  //writeToSDCard(sensor);
-  //RTC_init();
-  
-  Serial.println("-------------------------------");
+
+  Serial.println(F("-------------------------------"));
 }
 
 void loop() {
-  if (sensor.isReadyForReading()){   
+  if (sensor.isReadyForReading()){
     sensor.readData();
-    
+
     //SD_init();
     writeToSDCard(sensor);
-   
     delay(500);
-   
-    boolean connected = connectToNetwork(ssid, pass); 
+
+    boolean connected = connectToNetwork(ssid, pass);
 
     if (connected){
-      sendToPushingBox(sensor);
+      uploadData();
+      //logData();
+      //SD.remove(TO_UPLOAD_FILENAME);
     }
 
-    Serial.println("-------------------------------");
+    Serial.println(F("-------------------------------"));
   }
-  
+
   delay(1000);
 }
 
 boolean writeToSDCard(Decagon5TE sensor_data){
-  //last_sd_time = millis();
- 
   RTC_init();
-  
+
   String dataString = "";
-  
+
   dataString += ReadTimeDate();
   dataString += ",";
   dataString += doubleToString(sensor_data.getDielectricPermittivity());
@@ -64,13 +62,15 @@ boolean writeToSDCard(Decagon5TE sensor_data){
   dataString += doubleToString(sensor_data.getElectricalConductivity());
   dataString += ",";
   dataString += doubleToString(sensor_data.getTemperature());
+  dataString += ",";
 
-  Serial.println("I'm going to write to the SD card");
+  Serial.println(F("I'm going to write to the SD card"));
 
+  //SD_init();
   SPI.setDataMode(SPI_MODE0);
-  // open the file. note that only one file can be open at a time,
+  // open the file. note that only one file can be open at a time, 
   // so you have to close this one before opening another.
-  File dataFile = SD.open("datalog.txt", FILE_WRITE);
+  File dataFile = SD.open(TO_UPLOAD_FILENAME, FILE_WRITE);
 
   delay(500);
 
@@ -81,38 +81,98 @@ boolean writeToSDCard(Decagon5TE sensor_data){
     dataFile.close();
     return true;
     // print to the serial port too:
-  }  
+  }
   // if the file isn't open, pop up an error:
   else {
-    Serial.println("error opening to_upload.txt");
+    Serial.print(F("error opening "));
+    Serial.println(TO_UPLOAD_FILENAME);
     dataFile.close();
     return false;
   }
 
-  //SPI.setDataMode(SPI_MODE1);  
+//SPI.setDataMode(SPI_MODE1);
 }
 
-boolean sendToPushingBox(Decagon5TE sensor){
-  
-  Serial.println("I'm going to upload to the spreadsheet");
-  
-  //File dataFile = SD.open("toupload.txt", FILE_READ);
-  
+void logData(){
+  //SD_init();
+  File to_upload_file = SD.open(TO_UPLOAD_FILENAME, FILE_READ);
+  File log_file = SD.open(ARCHIVE_FILENAME, FILE_WRITE);
+
+  delay(500);
+
+  if (to_upload_file && log_file){
+    while (to_upload_file.available()){
+      log_file.print((char)to_upload_file.read());
+    }
+  }
+
+  to_upload_file.close();
+  log_file.close();
+}
+
+void uploadData(){
+  File to_upload_file = SD.open(TO_UPLOAD_FILENAME, FILE_READ);
+
+  delay(500);
+
+  if (to_upload_file) {
+    while(to_upload_file.read() != ',');
+    String permittivity = "";
+    String conductivity = "";
+    String temperature = "";
+    while(char data = to_upload_file.read()){
+      if (data == ','){
+        break;
+      }
+      permittivity += data;
+    }
+    Serial.print(F("Permittivity: "));
+    Serial.println(permittivity);
+    
+    while(char data = to_upload_file.read()){
+      if (data == ','){
+        break;
+      }
+      conductivity += data;
+    }
+
+    Serial.print(F("Conductivity: "));
+    Serial.println(conductivity);
+    
+    while(char data = to_upload_file.read()){
+      if (data == ','){
+        break;
+      }
+    temperature += data;
+    }
+    Serial.print(F("Temperature: "));
+    Serial.println(temperature);
+
+    String data = "&permittivity=";
+    data += permittivity;
+    data += "&conductivity=";
+    data += conductivity;
+    data += "&temperature=";
+    data += temperature;
+
+    if (sendToPushingBox(data)){
+      to_upload_file.close();
+      logData();
+      SD.remove(TO_UPLOAD_FILENAME);
+    }
+  }
+}
+
+boolean sendToPushingBox(String data) {
+
+  Serial.println(F("I'm going to upload to the spreadsheet"));
+
   boolean success = false;
-  
-  String data = "&permittivity=";
-  data += doubleToString(sensor_data.getDielectricPermittivity());
-  data += "&conductivity=";
-  data += doubleToString(sensor_data.getElectricalConductivity());
-  data += "&temperature=";
-  data += doubleToString(sensor_data.getTemperature());
-  
-  //char data_buffer[data.length()];  
-  
-  Serial.println("Starting connection to server...");
-  
+
+  Serial.println(F("Starting connection to server..."));
+
   if (client.connect("api.pushingbox.com", 80)) {
-    Serial.println("connected to server");
+    Serial.println(F("connected to server"));
     // Make a HTTP request:
     //client.println("GET /pushingbox?devid=vFB218A4E2233052&permittivity=1.52&conductivity=23.02 HTTP/1.1");
     client.print("GET /pushingbox?devid=");
@@ -122,43 +182,41 @@ boolean sendToPushingBox(Decagon5TE sensor){
     client.println("Host: api.pushingbox.com");
     client.println("Connection: close");
     client.println();
-    
+
     success = true;
-    Serial.println("Upload Successful");
+    Serial.println(F("Upload Successful"));
   }
   else {
-    success = false;  
-    Serial.println("Upload Failed");
+    success = false;
+    Serial.println(F("Upload Failed"));
   }
-  
+
   client.flush();
   client.stop();
-  
-  //last_upload_time = millis();
-  
-  return success; 
+
+  return success;
 }
 
 boolean connectToNetwork(char ssid[], char password[]){
   status = WiFi.status();
-  
+
   if (WiFi.status() == WL_NO_SHIELD) {
-    Serial.println("WiFi shield not present"); 
+    Serial.println(F("WiFi shield not present"));
     // don't continue:
     while(true);
-  } 
+  }
   int failures = 0;
   // attempt to connect to Wifi network:
   while (status != WL_CONNECTED && failures < 2) {
-  //Serial.println(failures); 
-    Serial.print("Attempting to connect to WPA SSID: ");
+    //Serial.println(failures);
+    Serial.print(F("Attempting to connect to WPA SSID: "));
     Serial.println(ssid);
-  // Connect to WPA/WPA2 network:    
+    // Connect to WPA/WPA2 network:
     status = WiFi.begin(ssid, pass);
     ++failures;
   }
   if (status) {
-    Serial.print("Connected to ");
+    Serial.print(F("Connected to "));
     Serial.println(WiFi.SSID());
   }
   return status;
@@ -166,18 +224,17 @@ boolean connectToNetwork(char ssid[], char password[]){
 
 String doubleToString(double input){
   char input_buffer[10] = {'/0'};
-  return dtostrf(input, 2, 2, input_buffer);  
+  return dtostrf(input, 2, 2, input_buffer);
 }
 
 int RTC_init(){
-  
   pinMode(RTC_CHIP_SELECT,OUTPUT); // chip select
   // start the SPI library:
   SPI.begin();
-  SPI.setBitOrder(MSBFIRST); 
-  SPI.setDataMode(SPI_MODE1); // both mode 1 & 3 should work 
-  //set control register 
-  digitalWrite(RTC_CHIP_SELECT, LOW);  
+  SPI.setBitOrder(MSBFIRST);
+  SPI.setDataMode(SPI_MODE1); // both mode 1 & 3 should work
+  //set control register
+  digitalWrite(RTC_CHIP_SELECT, LOW);
   SPI.transfer(0x8E);
   SPI.transfer(0x60); //60= disable Osciallator and Battery SQ wave @1hz, temp compensation, Alarms disabled
   digitalWrite(RTC_CHIP_SELECT, HIGH);
@@ -185,40 +242,40 @@ int RTC_init(){
 }
 
 boolean SD_init(){
- SPI.setDataMode(SPI_MODE0);
- Serial.println("Initializing SD card...");
+  SPI.setDataMode(SPI_MODE0);
+  Serial.println(F("Initializing SD card..."));
   // make sure that the default chip select pin is set to
   // output, even if you don't use it:
   pinMode(10, OUTPUT);
-  
+
   // see if the card is present and can be initialized:
   if (!SD.begin(SD_CHIP_SELECT)) {
-    Serial.println("Card failed, or not present");
+    Serial.println(F("Card failed, or not present"));
     // don't do anything more:
     return false;
   }
-  Serial.println("card initialized.");
+  Serial.println(F("card initialized."));
   return true;
 }
 
 String ReadTimeDate(){
   String temp;
-  int TimeDate [7]; //second,minute,hour,null,day,month,year		
+  int TimeDate [7]; //second,minute,hour,null,day,month,year
   for(int i=0; i<=6;i++){
     if(i==3)
     i++;
     digitalWrite(RTC_CHIP_SELECT, LOW);
-    SPI.transfer(i+0x00); 
-    unsigned int n = SPI.transfer(0x00);        
+    SPI.transfer(i+0x00);
+    unsigned int n = SPI.transfer(0x00);
     digitalWrite(RTC_CHIP_SELECT, HIGH);
-    int a=n & B00001111;    
-    if(i==2){	
+    int a=n & B00001111;
+    if(i==2){
       int b=(n & B00110000)>>4; //24 hour mode
       if(b==B00000010)
-	b=20;        
+        b=20;
       else if(b==B00000001)
-	b=10;
-	TimeDate[i]=a+b;
+        b=10;
+      TimeDate[i]=a+b;
     }
     else if(i==4){
       int b=(n & B00110000)>>4;
@@ -232,9 +289,9 @@ String ReadTimeDate(){
       int b=(n & B11110000)>>4;
       TimeDate[i]=a+b*10;
     }
-    else{	
+    else{
       int b=(n & B01110000)>>4;
-      TimeDate[i]=a+b*10;	
+      TimeDate[i]=a+b*10;
     }
   }
   temp.concat(TimeDate[4]);
